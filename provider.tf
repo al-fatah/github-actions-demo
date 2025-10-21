80efdf113e91b80ba76d364ee7d28ff451cb1652
al-fatah <alfatah.din@gmail.com>
2025-10-21 20:56:03 +0800

    update tf

--- provider.tf
+++ provider.tf
@@ -10,3 +10,11 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce11-tfstate"
+    key    = "alfatah-tf-workspace-act.tfstate"   #Change the value of this to yourname-tf-workspace-act.tfstate for  example
+    region = "us-east-1"
+  }
+}
